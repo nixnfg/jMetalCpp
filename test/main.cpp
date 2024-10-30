960a249c452ab85314af30cebd8b7cd607509200
nixnfg <nixnfg@live.com>
2024-10-30 09:52:19 +0800

    msvc

--- test/main.cpp
+++ test/main.cpp
@@ -1,24 +1,4 @@
-//  NSGAII_main.cpp
-//
-//  Author:
-//       Esteban López-Camacho <esteban@lcc.uma.es>
-//
-//  Copyright (c) 2011 Antonio J. Nebro, Juan J. Durillo
-//
-//  This program is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as published by
-//  the Free Software Foundation, either version 3 of the License, or
-//  (at your option) any later version.
-//
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU Lesser General Public License for more details.
-//
-//  You should have received a copy of the GNU Lesser General Public License
-//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
+#include <math.h>
 #include <Problem.h>
 #include <Solution.h>
 #include <SBXCrossover.h>
@@ -26,115 +6,147 @@
 #include <BinaryTournament2.h>
 #include <iostream>
 #include <NSGAII.h>
-#include <ProblemFactory.h>
 #include <string.h>
 #include <time.h>
+//#include "wrs_sub.h"
 
-
-/**
- * Class implementing the NSGA-II algorithm.
- * This implementation of NSGA-II makes use of a QualityIndicator object
- *  to obtained the convergence speed of the algorithm. This version is used
- *  in the paper:
- *     A.J. Nebro, J.J. Durillo, C.A. Coello Coello, F. Luna, E. Alba
- *     "A Study of Convergence Speed in Multi-Objective Metaheuristics."
- *     To be presented in: PPSN'08. Dortmund. September 2008.
- *
- *   Besides the classic NSGA-II, a steady-state version (ssNSGAII) is also
- *   included (See: J.J. Durillo, A.J. Nebro, F. Luna and E. Alba
- *                  "On the Effect of the Steady-State Selection Scheme in
- *                  Multi-Objective Genetic Algorithms"
- *                  5th International Conference, EMO 2009, pp: 183-197.
- *                  April 2009)
- */
-int main(int argc, char ** argv) {
-
-	clock_t t_ini, t_fin;
-  
-  Problem   * problem   ; // The problem to solve
-  Algorithm * algorithm ; // The algorithm to use
-  Operator  * crossover ; // Crossover operator
-  Operator  * mutation  ; // Mutation operator
-  Operator  * selection ; // Selection operator
-
-  if (argc>=2) {
-    problem = ProblemFactory::getProblem(argc, argv);
-    cout << "Selected problem: " << problem->getName() << endl;
-  } else {
-    cout << "No problem selected." << endl;
-    cout << "Default problem will be used: Fonseca" << endl;
-    problem = ProblemFactory::getProblem(const_cast<char *>("Fonseca"));
-  }
-  
-//  QualityIndicator * indicators ; // Object to get quality indicators
-//	indicators = NULL ;
-
-	algorithm = new NSGAII(problem);
-
-  // Algorithm parameters
-  int populationSize = 100;
-  int maxEvaluations = 25000;
-  algorithm->setInputParameter("populationSize",&populationSize);
-  algorithm->setInputParameter("maxEvaluations",&maxEvaluations);
-
-	// Mutation and Crossover for Real codification
-	map<string, void *> parameters;
-
-  double crossoverProbability = 0.9;
-  double crossoverDistributionIndex = 20.0;
-  parameters["probability"] =  &crossoverProbability;
-  parameters["distributionIndex"] = &crossoverDistributionIndex;
-  crossover = new SBXCrossover(parameters);
-
-	parameters.clear();
-  double mutationProbability = 1.0/problem->getNumberOfVariables();
-  double mutationDistributionIndex = 20.0;
-  parameters["probability"] = &mutationProbability;
-  parameters["distributionIndex"] = &mutationDistributionIndex;
-  mutation = new PolynomialMutation(parameters);
-
-	// Selection Operator
-	parameters.clear();
-	selection = new BinaryTournament2(parameters);
-
-	// Add the operators to the algorithm
-	algorithm->addOperator("crossover",crossover);
-	algorithm->addOperator("mutation",mutation);
-	algorithm->addOperator("selection",selection);
-
-	// Add the indicator object to the algorithm
-//	algorithm->setInputParameter("indicators", indicators) ;
-
-	// Execute the Algorithm
-	t_ini = clock();
-	SolutionSet * population = algorithm->execute();
-	t_fin = clock();
-	double secs = (double) (t_fin - t_ini);
-	secs = secs / CLOCKS_PER_SEC;
-
-	// Result messages
-	cout << "Total execution time: " << secs << "s" << endl;
-	cout << "Variables values have been written to file VAR" << endl;
-	population->printVariablesToFile("VAR");
-	cout << "Objectives values have been written to file FUN" << endl;
-	population->printObjectivesToFile("FUN");
-  
-//  if (indicators != NULL) {
-//    cout << "Quality indicators" << endl;
-//    cout << "Hypervolume: " << indicators->getHypervolume(population) << endl;
-//    cout << "GD         : " << indicators->getGD(population) << endl;
-//    cout << "IGD        : " << indicators->getIGD(population) << endl;
-//    cout << "Spread     : " << indicators->getSpread(population) << endl;
-//    cout << "Epsilon    : " << indicators->getEpsilon(population) << endl;
+//#include <iostream>
+//#include <vector>
+//int main() {
+//    std::vector<int> numbers = {1, 2, 3, 4, 5};
+//    int sum = 0;
 //    
-//    int evaluations = *(int *) algorithm->getOutputParameter("evaluations");;
-//    cout << "Speed      : " << evaluations << " evaluations" << endl;
-//  } // if
+//    for(const auto& num : numbers) {
+//        sum += num;  // 设置断点测试
+//    }
+//    
+//    std::cout << "Sum: " << sum << std::endl;
+//    return 0;
+//}
 
-  delete selection;
-  delete mutation;
-  delete crossover;
-  delete population;
-  delete algorithm;
+class MyProblem : public Problem {
+public:
+    MyProblem() {
+        numberOfVariables_   = 10;
+        numberOfObjectives_  = 2;
+        numberOfConstraints_ = 2;
+        problemName_         = "MyProblem";
 
-} // main
+        lowerLimit_ = new double[numberOfVariables_];
+        upperLimit_ = new double[numberOfVariables_];
+
+        for (int i = 0; i < numberOfVariables_; i++) {
+            lowerLimit_[i] = 0.0;
+            upperLimit_[i] = 1.0;
+        }
+        solutionType_ = new RealSolutionType(this);
+    }
+    ~MyProblem()
+    {
+        delete [] lowerLimit_;
+        delete [] upperLimit_;
+    }
+    void evaluate(Solution *solution) {
+        double *x = new double[numberOfVariables_];
+        for (int i = 0; i < numberOfVariables_; i++)
+            x[i] = solution->getDecisionVariables()[i]->getValue();
+
+        double f1 = 0.0;
+        for (int i = 0; i < numberOfVariables_; i++)
+            f1 += x[i] * x[i];
+
+        double f2 = 0.0;
+        for (int i = 0; i < numberOfVariables_; i++)
+            f2 += (x[i] - 2.0) * (x[i] - 2.0);
+
+        solution->setObjective(0, f1);
+        solution->setObjective(1, f2);
+
+        delete[] x;
+    }
+    void evaluateConstraints(Solution *solution)
+    {
+        double *constraint = new double[getNumberOfConstraints()];
+        double *x = new double[numberOfVariables_];
+        for(int i=0;i<numberOfVariables_;i++)
+        {
+            x[i]=solution->getDecisionVariables()[i]->getValue();
+        }
+        // Constraints
+        constraint[0] = 0.0;
+        constraint[1] = 0.0;
+        for (int i = 0; i < numberOfVariables_; i++) {
+            constraint[0] += x[i];
+            constraint[1] += x[i] * x[i];
+        }
+        constraint[0] = constraint[0] - 5.0;
+        constraint[1] = constraint[1] - 9.0;
+        int number = 0;
+        double total = 0.0;
+        for(int i=0; i<this->getNumberOfConstraints();i++)
+        {
+            // 这个可以判断
+            if(constraint[i]>0.0)
+            {
+                number++;
+                total+=constraint[i];
+            }
+        }
+        solution->setOverallConstraintViolation(total);
+        solution->setNumberOfViolatedConstraints(number);
+
+        delete x;
+    }
+};
+
+int main() {
+    Problem   * problem   = new MyProblem();
+    Algorithm * algorithm = new NSGAII(problem);
+
+    // Algorithm parameters
+    int populationSize = 100;
+    int maxEvaluations = 25000;
+    algorithm->setInputParameter("populationSize", &populationSize);
+    algorithm->setInputParameter("maxEvaluations", &maxEvaluations);
+
+    // Mutation and Crossover for Real codification
+    map<string, void *> parameters;
+    double crossoverProbability = 0.9;
+    double crossoverDistributionIndex = 20.0;
+    parameters["probability"] = &crossoverProbability;
+    parameters["distributionIndex"] = &crossoverDistributionIndex;
+    Crossover * crossover = new SBXCrossover(parameters);
+
+    parameters.clear();
+    double mutationProbability = 1.0 / problem->getNumberOfVariables();
+    double mutationDistributionIndex = 20.0;
+    parameters["probability"] = &mutationProbability;
+    parameters["distributionIndex"] = &mutationDistributionIndex;
+    Mutation * mutation = new PolynomialMutation(parameters);
+    parameters.clear();
+    Selection * selection = new BinaryTournament2(parameters);
+
+    // Add the operators to the algorithm
+    algorithm->addOperator("crossover", crossover);
+    algorithm->addOperator("mutation", mutation);
+    algorithm->addOperator("selection",selection);
+
+    // Execute the Algorithm
+    SolutionSet * population = algorithm->execute();
+
+    // Result messages
+    //cout << "Total execution time: " << algorithm->getExecutionTime() << "ms" << endl;
+    cout << "Variables values have been written to file VAR" << endl;
+    population->printVariablesToFile("out_VAR.txt");
+    cout << "Objectives values have been written to file FUN" << endl;
+    population->printObjectivesToFile("out_FUN.txt");
+    
+    delete selection;
+    delete crossover;
+    delete mutation;
+    delete population;
+    delete algorithm;
+
+
+    return 0;
+}
